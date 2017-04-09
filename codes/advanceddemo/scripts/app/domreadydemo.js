0b782e754270afa727fb46f365ec8e4bccc0e438
CodingWorker <dy10010@sina.cn>
2017-04-09 20:56:46 +0800

    ss

---
+++ codes/advanceddemo/scripts/app/domreadydemo.js
@@ -0,0 +1,9 @@
+//moduleId -> domReady
+// define(['domReady!'],
+// 	function(domReady){
+// 		domReady(
+// 			function(dr){
+// 				console.log(dr);
+// 			});
+// 	}
+// );
--- codes/advanceddemo/scripts/main.js
+++ codes/advanceddemo/scripts/main.js
@@ -1,11 +1,13 @@
 requirejs.config({
 	baseUrl:'scripts',
 	paths:{
+		app:'app',
 		jquery:'../../libs/jquery'
-	}
+	},
+	waitSeceds:10
 });
 
-define(['jquery'],
+define(['jquery', 'app/index', 'app/domreadydemo'],
 	function($){
 		alert($);
 	});
